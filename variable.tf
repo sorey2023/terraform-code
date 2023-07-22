36edf118b1c776ba9b2a307f8dd8dd815a098102
Sorel <sorey2023>
2023-07-22 16:40:48 -0700

    terrafrm

---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  default = "default"
+
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+
+variable "Team" {
+  default = "dev"
+
+}
+
+variable "protocol" {
+  default = "email"
+
+}
+
+variable "endpoint" {
+  default = "ddvffe@gmail.com"
+
+}
